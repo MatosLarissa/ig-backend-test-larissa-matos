068a98c4a07ed2a5ff9d9e1a35460e744fc320a6
Larissa Matos <larissamatos88@outlook.com>
2022-04-08 10:36:35 -0300

    Business-Repository-Task-getAllTaskByUser

--- src/Business/Task/TaskRepository.ts
+++ src/Business/Task/TaskRepository.ts
@@ -2,4 +2,5 @@ import { Task } from "../../Model/Task/Task"
 
 export interface TaskRepository {
     createTask(task: Task): Promise<Task>
+    getAllTaskByUser(id: string): Promise<Task | null>
 }
