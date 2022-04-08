efe251b510ad6caea40b355b6e38332f374f4f93
Larissa Matos <larissamatos88@outlook.com>
2022-04-08 10:22:17 -0300

    Router-createTask

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { app } from "./Data/server"
+import { taskRouter } from "./Router/Task/taskRouter"
 import { userRouter } from "./Router/User/userRouter"
 
 app.use('/user', userRouter)
+app.use('/task', taskRouter)
