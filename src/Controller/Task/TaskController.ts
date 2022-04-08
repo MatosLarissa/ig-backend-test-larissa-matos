bb62ed53eaae66499f91849338be45fec85bb8a7
Larissa Matos <larissamatos88@outlook.com>
2022-04-08 10:19:40 -0300

    Controller-createTask

---
+++ src/Controller/Task/TaskController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import TaskBusiness from "../../Business/Task/TaskBusiness";
+import TaskDatabase from "../../Data/Task/TaskDatabase";
+import { TaskInputDTO } from "../../Model/Task/Task";
+
+export class TaskController {
+
+    private taskBusiness: TaskBusiness
+    constructor() {
+        this.taskBusiness = new TaskBusiness(new TaskDatabase)
+    }
+
+    createTask = async (req: Request, res: Response) => {
+
+        const taskInput: TaskInputDTO = {
+            token: req.headers.authorization,
+            title: req.body.title,
+            done: req.body.done,
+            date: req.body.date
+        }
+
+        try {
+
+            const token = await this.taskBusiness.createTask(taskInput)
+
+            res.status(201).send({
+                message: "Task created successfully!",
+                token
+            })
+
+        } catch (error) {
+            const { statusCode, message } = error
+            res.status(statusCode || 400).send({ message })
+        }
+
+    }
+}
