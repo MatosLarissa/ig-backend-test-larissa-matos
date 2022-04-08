813f9a293433c526bb89b4bee99959032c3d6122
Larissa Matos <larissamatos88@outlook.com>
2022-04-07 22:01:46 -0300

    update name finished

--- src/Business/User/UserRepository.ts
+++ src/Business/User/UserRepository.ts
@@ -4,4 +4,6 @@ export interface UserRepository {
     createUser(user: User): Promise<User>
     getUserByEmail(email: string): Promise<User | null>
     getUserByCpf(cpf: number): Promise<User | null>
+    getUserById(id: string): Promise<User | null>
+    updateName(id: string, name: string, lastName: string): Promise<User>
 }
