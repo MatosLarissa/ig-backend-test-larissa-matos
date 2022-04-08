ac9d411574ce78d8610254a7fae5159a9f8ed599
Larissa Matos <larissamatos88@outlook.com>
2022-04-07 21:47:29 -0300

    login is working

--- src/Controller/User/UserController.ts
+++ src/Controller/User/UserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import UserBusiness from "../../Business/User/UserBusiness";
 import UserDatabase from "../../Data/User/UserDatabase";
-import {  SignupInputDTO } from "../../Model/User/User";
+import {  LoginInputDTO, SignupInputDTO } from "../../Model/User/User";
 
 export class UserController {
 
@@ -26,6 +26,25 @@ export class UserController {
 
             res.status(201).send({ token })
 
+        } catch (error) {
+            const { statusCode, message } = error
+            res.status(statusCode || 400).send({ message })
+        }
+
+    }
+
+    login = async (req: Request, res: Response) => {
+
+        const loginInput: LoginInputDTO = {
+            email: req.body.email,
+            password: req.body.password
+        }
+
+        try {
+
+            const token = await this.userBusiness.login(loginInput)
+
+            res.status(200).send({ token })
         } catch (error) {
             const { statusCode, message } = error
             res.status(statusCode || 400).send({ message })
