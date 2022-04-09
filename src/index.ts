5b6c85e7c31a8eae89410d2f92b9a9294f5f6271
Larissa Matos <larissamatos88@outlook.com>
2022-04-09 15:20:15 -0300

    script

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-import { app } from "./Data/server"
+import express from "express";
+import cors from "cors"
+import dotenv from "dotenv"
+import { AddressInfo } from "net";
 import { taskRouter } from "./Router/Task/taskRouter"
 import { userRouter } from "./Router/User/userRouter"
 
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
 app.use('/user', userRouter)
 app.use('/task', taskRouter)
+
+export const server = app.listen(process.env.PORT || 3333, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`)
+  } else {
+    console.error(`Failure upon starting server.`)
+  }
+})
