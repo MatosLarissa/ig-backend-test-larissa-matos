d840bd414defab64382864bf72e66848e21e8f24
Larissa Matos <larissamatos88@outlook.com>
2022-04-09 15:26:50 -0300

    tsnd-script

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,11 @@ app.use('/user', userRouter)
 app.use('/task', taskRouter)
 
 export const server = app.listen(process.env.PORT || 3333, () => {
-  if (server) {
-    const address = server.address() as AddressInfo;
-    console.log(`Server is running in http://localhost:${address.port}`)
-  } else {
-    console.error(`Failure upon starting server.`)
-  }
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`)
+    } else {
+        console.error(`Failure upon starting server.`)
+
+    }
 })
